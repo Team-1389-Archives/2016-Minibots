2d07e24414303a261793aa2497a225eb9808a7f1
Joshua Engels <joshuaengels@yahoo.com>
2016-10-01 18:14:02 -0400

    Finished ESP code, started and tested with Arduino code

---
+++ 2016-Minibots/ArduinoCode/ArduinoCode.ino
@@ -0,0 +1,13 @@
+
+void setup(){
+  Serial.begin(115200);
+}
+
+void loop(){
+ while (Serial.available() > 0){
+    char c = Serial.read();
+    if(c != 10 && c != 13)
+      Serial.println(c);
+  }  
+}
+
--- 2016-Minibots/ESPCode/ESPCode.ino
+++ 2016-Minibots/ESPCode/ESPCode.ino
@@ -1,17 +1,19 @@
-//THIS WORKS OH YEAHH
-
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
 unsigned int localUdpPort = 4210;  // local port to listen on
 WiFiUDP Udp;
+IPAddress remoteIP;
+int remotePort = 0;
 
+//IP Adress: 192.168.4.1
 void setup()
 {
   Serial.begin(115200);
-  Serial.println();
-
-  Serial.print("Setting soft-AP ... ");
+  WiFi.softAP("ESPFinalTest");
+  
+  //Serial.println();
+  /*Serial.print("Setting soft-AP ... ");
   boolean result = WiFi.softAP("ESPFinalTest");
   if(result == true)
   {
@@ -20,23 +22,84 @@ void setup()
   else
   {
     Serial.println("Failed!");
-  }
+  }*/
 
    Udp.begin(localUdpPort);
 
-   IPAddress myIP = WiFi.softAPIP();
+   /*IPAddress myIP = WiFi.softAPIP();
    Serial.print("Listening on port: ");
    Serial.print(localUdpPort);
    Serial.print(", IP Adress: ");
    Serial.println(myIP);
+   */
 }
 
 void loop()
 {
 
+  //Looks for packets, sends to ESP
   int packetSize = Udp.parsePacket();
-  if (packetSize){ //Packet not empty
-    Serial.println(packetSize);
+  if (packetSize > 0){ //Packet not empty
+    remoteIP = Udp.remoteIP();
+    remotePort = Udp.remotePort();
+
+    char incomingPacket[packetSize];
+    int len = Udp.read(incomingPacket, packetSize);
+    
+    //Truncates the end signifier bytes
+    if (len > 0)
+    {
+      incomingPacket[len] = 0; 
+    }
+
+    //Send packet to ESP
+    Serial.println(incomingPacket);
+
   }
+
+   //Gets input from arduino and sends to computer over UDP
+   String packetToSend = "";
+   while(Serial.available() > 0){
+      char nextChar = Serial.read();
+      
+      //Sends packet if next char is carriage return
+      if(nextChar == '\n'){
+        sendPacket(packetToSend);
+        packetToSend = "";
+      } 
+      else{
+        packetToSend.concat(nextChar);
+      }
+      delay(10); //Need this for some reason, think about later
+      
+  }
+
+
+  
   
 }
+
+//Sends packet to last computer a packet was recieved from
+void sendPacket(String packet){  
+  
+  //Creates byte array
+  int bufferLength = packet.length() + 1;
+  byte buf[bufferLength];
+  packet.getBytes(buf, bufferLength);
+
+  /*
+  Serial.print("Packet Sent: ");
+  Serial.println(packet);
+  Serial.print("IP Adress: ");
+  Serial.print(remoteIP);
+  Serial.print(", Port: ");
+  Serial.println(remotePort);
+  */
+
+  Udp.beginPacket(remoteIP, remotePort);
+  Udp.write(buf, bufferLength);
+  Udp.endPacket();
+  
+}
+
+
