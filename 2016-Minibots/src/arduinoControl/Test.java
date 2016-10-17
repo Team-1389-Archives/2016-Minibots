1f98b77337f5869ec9155f0e5f4b2fb2e465b71b
Joshua Engels <joshuaengels@yahoo.com>
2016-10-16 22:22:10 -0400

    First tests done

--- 2016-Minibots/src/arduinoControl/Test.java
+++ 2016-Minibots/src/arduinoControl/Test.java
@@ -1,21 +1,64 @@
 package arduinoControl;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 
-public class Test {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class Test extends JPanel implements KeyListener{
+
+	static int change = 0;
 	public static void main(String[] args) throws IOException, InterruptedException{
 		UDPServer.initializeServer();
 		
+		Test t = new Test();
+		JFrame f = new JFrame();
+		f.setSize(400, 400);
+		f.getContentPane().add(t);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		
 		Servo s = new Servo(3);
-		for(int i = 0; i < 10; i++){
-			s.send(180);
-			Thread.sleep(500);
-			s.send(0);
-			Thread.sleep(500);
+		s.send(90);
+		while(true){
+			s.send(s.getLastValue() + change);
+			change = 0;
+			Thread.sleep(10);
 		}
 
+	}
+
+	Test(){
+		addKeyListener(this);
+	}
+	
+    public void addNotify() {
+        super.addNotify();
+        requestFocus();
+    }
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		if(arg0.getKeyChar() == 's'){
+			change = -5;
+		}
+		else if(arg0.getKeyChar() == 'd'){
+			change = 5;
+		}
+		System.out.println(arg0.getKeyChar());
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		System.out.println(arg0.getKeyChar());
+		
 	}
 }
--- 2016-Minibots/src/arduinoControl/Write.java
+++ 2016-Minibots/src/arduinoControl/Write.java
@@ -6,11 +6,13 @@ import arduinoControl.Constants.TYPE;
 
 abstract class Write extends Pin{
 
+	private int lastValueSent = 0;
 	Write(TYPE type, int pin) {
 		super(type, pin);
 	}
 
 	protected void send(int value, String type) throws IOException {
+		lastValueSent = value;
 		UDPServer.sendString(type + "|" + pin + "|" + value + ";");
 	}
 
@@ -23,6 +25,9 @@ abstract class Write extends Pin{
 
 	protected abstract String getWriteType();
 	
+	public int getLastValue(){
+		return lastValueSent;
+	}
 	
 
 }
